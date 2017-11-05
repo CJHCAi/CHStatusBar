53abf5836a5029edd367a792e6c8686db986abe0
CJH5209 <1312656992@qq.com>
2017-11-05 23:21:47 +0800

    增加注释
    充满恒心

--- CHStatusBarDemo/CHStatusBarDemo/ViewController.m
+++ CHStatusBarDemo/CHStatusBarDemo/ViewController.m
@@ -5,10 +5,33 @@
 //  Created by 七啸网络 on 2017/11/5.
 //  Copyright © 2017年 CWM. All rights reserved.
 //
+//pod trunk register  1312656992@qq.com 'CJH' --description='the MAC of CJH'
+/**
+ 
+ Pod::Spec.new do |s|
+ s.name         = "CHExtension"
+ s.version      = "0.0.1"
+ s.summary      = "The fastest and most convenient conversion between JSON and model"
+ s.homepage     = "https://github.com/CJHCAi/CHStatusBar"
+ s.license      = "MIT"
+ s.author             = { "CJH" => "1312656992@qq.com" }
+ s.social_media_url   = "http://weibo.com/exceptions"
+ s.source       = { :git => "https://github.com/CJHCAi/CHExtension.git", :tag => s.version }
+ s.source_files  = "CHExtensionExample/CHExtensionExample/CHExtension"
+ s.requires_arc = true
+ end
+ 
+ 
+ pod spec lint CHExtension.podspec
+ 
+ 
+ pod trunk push CHExtension.podspec
+ */
 
 #import "ViewController.h"
 #import "CHStatusBarHUD.h"
 @interface ViewController ()
+
 
 @end
 
